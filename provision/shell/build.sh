87085b7c6d111c996804a32bb539833ec6b6d74d
Pietro T <pietro.tortorici@gmail.com>
2018-04-23 23:35:42 -0400

    Updated build script for latest version of box.

--- provision/shell/build.sh
+++ provision/shell/build.sh
@@ -20,25 +20,22 @@ USERNAME="vagrant"
 USERGROUP="vagrant"
 
 # Install repositories.
-sudo add-apt-repository -y ppa:ondrej/apache2
-sudo add-apt-repository -y ppa:ondrej/php # Most requested extensions (supports php 7.0).
+LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/apache2
+LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php # Most requested extensions (supports php 7.0).
 
 # Update VM & add some needed apps & dependencies.
 apt-get -y update
-apt-get -y upgrade
-apt-get -y install wget vim git curl htop tmux ruby python-software-properties
+apt-get -y --allow-unauthenticated upgrade
+apt-get -y --allow-unauthenticated install wget vim git curl htop tmux ruby python-software-properties
 
 # Setup locale.
-apt-get -y install language-pack-en-base
+apt-get -y --allow-unauthenticated install language-pack-en-base
 locale-gen en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
 # Synchronize the System Clock.
-apt-get -y install ntp ntpdate
-
-# Install composer.
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+apt-get -y --allow-unauthenticated install ntp ntpdate
 
 # ---------------------------------------
 #          MySQL Setup
@@ -51,7 +48,7 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password_again passwo
 # Install MySQL 5.7.
 # Luckily MySql 5.7 is available in the default repositories. 5.7 IS NOT BACKWARD COMPATIBLE.
 # Ensure to enter a root password otherwise a temporary one is created - 5.7 requirement.
-apt-get install -y mysql-server mysql-client
+apt-get install -y --allow-unauthenticated mysql-server mysql-client
 mysql --version
 
 # ---------------------------------------
@@ -59,14 +56,14 @@ mysql --version
 # ---------------------------------------
 
 # Install packages.
-apt-get install -y apache2
+apt-get install -y --allow-unauthenticated apache2
 
 # See available mods.
 #ll /etc/apache2/mods-available | grep proxy
 
 # Install mods.
 # https://wiki.apache.org/httpd/PHP-FPM
-apt-get install -y mod_proxy proxy mod_proxy_fcgi
+apt-get install -y --allow-unauthenticated mod_proxy proxy mod_proxy_fcgi
 
 # Enable mods.
 a2enmod actions alias rewrite ssl include headers setenvif proxy_fcgi
@@ -79,7 +76,7 @@ service apache2 restart
 # ---------------------------------------
 
 # Install packages.
-apt-get install -y php7.0 php7.0-fpm
+apt-get install -y --allow-unauthenticated php7.0 php7.0-fpm
 php -v
 
 sed -i "s/user =.*/user = $USERNAME/" /etc/php/7.0/fpm/pool.d/www.conf
@@ -87,7 +84,7 @@ sed -i "s/group =.*/group = $USERGROUP/" /etc/php/7.0/fpm/pool.d/www.conf
 sed -i "s/listen =.*/listen = \/var\/run\/php7.0-fpm.sock/" /etc/php/7.0/fpm/pool.d/www.conf
 
 # Install extensions.
-apt-get install -y php7.0-cli php7.0-common php7.0-curl php7.0-gd php7.0-intl php7.0-json php7.0-mcrypt php-memcached php7.0-xmlrpc php7.0-zip php7.0-mbstring php7.0-xml php7.0-mysql
+apt-get install -y --allow-unauthenticated php7.0-cli php7.0-common php7.0-curl php7.0-gd php7.0-intl php7.0-json php7.0-mcrypt php-memcached php7.0-xmlrpc php7.0-zip php7.0-mbstring php7.0-xml php7.0-mysql
 phpenmod zip mbstring
 
 # Trigger changes.
@@ -157,8 +154,10 @@ service apache2 restart
 # ---------------------------------------
 
 # Install packages.
-apt-get install -y redis-server
+apt-get install -y --allow-unauthenticated redis-server
 
 # Install NodeJS & NPM.
-apt-get install -y nodejs nodejs-legacy npm
+apt-get install -y --allow-unauthenticated nodejs nodejs-legacy npm
 
+# Install composer.
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
